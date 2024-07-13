7ed7931cff1023a73a0b5ef347168073c4d9253a
66022804 <150765954+66022804@users.noreply.github.com>
2024-07-13 16:24:15 +0700

    uuu

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-    <div>
-        <Buttontest>
-            <font-awesome-icon :icon="['fab', 'facebook']" style="font-size: 30px" />
-            Click me
-        </Buttontest>
-    </div>
+  <div>
+    <Buttontest>
+      <font-awesome-icon :icon="['fab', 'facebook']" style="font-size: 30px" />
+      Click me jjjjj
+    </Buttontest>
+  </div>
 </template>
-  
