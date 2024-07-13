8c6bb0e737b562db63c9aaddb790fdc200a06ecb
korarit <krtza1@gmail.com>
2024-07-13 15:39:40 +0700

    first upload

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Buttontest>
+            <font-awesome-icon :icon="['fab', 'facebook']" style="font-size: 30px" />
+            Click me
+        </Buttontest>
+    </div>
+</template>
+  
---
+++ pages/test.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <Buttontest>Test Button</Buttontest>
+    </div>
+</template>
+  
