d52355524c8fcf1eb08ad342e1da940a06c76c42
korarit <krtza1@gmail.com>
2024-07-15 22:18:12 +0700

    make footer compunent

---
+++ components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="min-w-full py-4 md:py-12 px-4 xl:px-16 2xl:px-[96px] bg-[#FFFFFF] shadow-[inset_0_3px_10px_-7px_rgba(0,0,0,0.5)] border-t">
+        <div class="min-w-full flex flex-col md:flex-row md:items-center  md:justify-between">
+            <div>
+                <p class="text-[5vw] md:text-[24px] xl:text-[24px] 2xl:text-[28px]">
+                    © 2024 - 2025 รวมพลคนนอนเยอะ team
+                </p>
+                <p class="text-[4vw] md:text-[16px] xl:text-[20px] 2xl:text-[24px]">
+                    Project is in the subject of Software engineer major University of Phayao
+                </p>  
+            </div>
+
+            <div class="w-fit flex items-center gap-5 mx-auto md:mx-0">
+                <img src="~/assets/icon/seup.svg" class="w-[14dvw] md:w-[8dvw] md:h-[8dvw] xl:h-[84px] xl:w-[84px]" />
+                <img src="~/assets/icon/university-up.svg" class="w-[16dvw] md:w-[12dvw] md:h-[12dvw] xl:h-[128px] xl:w-[96px]" />
+            </div>
+            
+            
+        </div>
+    </div>
+
+</template>
