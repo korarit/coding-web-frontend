27b7ed6a3363230574ca04e6bf04627496bc855d
korarit <krtza1@gmail.com>
2024-07-18 00:02:22 +0700

    add language dropdow

---
+++ components/Dropdown/EditorLang.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="py-2 px-2 flex gap-2 w-fit">
+    <div class="flex flex-col w-fit gap-x-2">
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">C</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">C++</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">C#</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">Rust</p>
+      </button>
+    </div>
+
+    <div class="min-h-full w-[1px] bg-[#bababa]"></div>
+
+    <div class="flex flex-col w-fit gap-x-2">
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">Java</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">Python 3</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">Python 2</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">Lua</p>
+      </button>
+    </div>
+
+    <div class="min-h-full w-[1px] bg-[#bababa]"></div>
+
+    <div class="flex flex-col w-fit gap-x-2">
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">JavaScript</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">TypeScript</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">PHP</p>
+      </button>
+      <button class="hover:bg-[#e0e0e0] w-[128px] rounded-md py-2 px-1">
+        <p class="text-[16px] text-left text-[#262626] leading-4">Go</p>
+      </button>
+    </div>
+  </div>
+</template>
