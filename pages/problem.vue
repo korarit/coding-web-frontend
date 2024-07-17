ec62fc5b7386f98ca9c6d3f3a077626331c57e3f
korarit <krtza1@gmail.com>
2024-07-18 00:52:34 +0700

    fix-layout-dymanic-problem-do

--- pages/problem.vue
+++ pages/problem.vue
@@ -4,7 +4,7 @@
         <div class="min-h-full h-fit px-4 xl:px-16 2xl:px-[96px] gap-x-5 gap-y-8 flex-grow">
           <!-- Your content here -->
             <splitpanes @resized="onResizedWidth" :style="`${isFullscreen ? 'height: calc(100dvh - 100px)' : 'height: calc(100dvh - 195px)'}`">
-                <pane :size="paneLeftWidth" class="min-w-9 rounded-md overflow-hidden drop-shadow-md pane-discription" ref="paneRef">
+                <pane :size="paneLeftWidth" class="min-w-9 rounded-md overflow-hidden drop-shadow-md pane-discription">
                     <div class="w-full h-full bg-[#FFFFFF] border border-[#B0B0B0] rounded-md overflow-hidden drop-shadow-md">
                         <div :class="`w-full bg-[#F6F6F6] border-b border-[#B0B0B0] flex ${isVerticalLeftMode ? 'h-full justify-between vertical-mode' : 'h-9 items-center justify-between px-1'}`">
                             <div class="flex h-fit items-center gap-[2px] w-full">
@@ -28,10 +28,10 @@
                                 <button class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                     <font-awesome-icon :icon="['fas', 'share']" class="text-[24px] text-[#6B6B6B]" />
                                 </button>
-                                <button @click="setWidthPaneLeft(0)" v-if="paneLeftWidth > 1.5" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
+                                <button @click="setWidthPaneLeft(0)" v-show="paneLeftWidth > 1.5" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                     <font-awesome-icon :icon="['fas', 'chevron-left']" class="text-[24px] text-[#6B6B6B]" />
                                 </button>
-                                <button @click="setWidthPaneLeft(null)" v-if="paneLeftWidth < 1.5" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
+                                <button @click="setWidthPaneLeft(null)" v-show="paneLeftWidth < 1.5" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                     <font-awesome-icon :icon="['fas', 'chevron-right']" class="text-[24px] text-[#6B6B6B]" />
                                 </button>
                             </div>
@@ -43,23 +43,25 @@
                     <splitpanes @resized="resizedHeight" horizontal>
                     <pane :size="paneEditor" class="min-h-9">
                         <div class="w-full h-full bg-[#FFFFFF] border border-[#B0B0B0] rounded-md overflow-hidden drop-shadow-md">
-                            <div class="w-full bg-[#F6F6F6] border-b border-[#B0B0B0] h-9 px-1 flex items-center justify-between">
+
+                            <div :class="`w-full bg-[#F6F6F6] border-b border-[#B0B0B0] flex  ${isVerticalRightMode ? 'h-full justify-between vertical-mode' : 'h-9 items-center justify-between px-1'}`">
                                 <div class="flex h-full items-center gap-[2px]">
-                                    <div class="hover:bg-[#e4e4e4] rounded-md h-8 px-2 flex items-center gap-2">
-                                        <font-awesome-icon :icon="['fas', 'code']" class="text-[20px] text-[#00C7A3]" />
+                                    <div :class="`hover:bg-[#e4e4e4] rounded-md flex items-center gap-2 ${isVerticalRightMode ? 'h-fit py-2' : 'h-8 px-2'}`">
+                                        <font-awesome-icon :icon="['fas', 'code']" :class="`text-[20px] text-[#00C7A3] ${isVerticalRightMode ? 'rotate-90	' : null}`" />
                                         <span class="text-[#000000] text-[16px] font-medium">Editor</span>
                                     </div>
                                 </div>
 
                                 <div class="flex items-center gap-[4px]">
-                                    <button  @click="() => setHeightEditor(0)" v-if="paneEditor > 1.5 && EditorHidden == false"  class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
+                                    <button  @click="() => setHeightEditor(0)" v-show="paneEditor > 1.5 && EditorHidden == false"  class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                         <font-awesome-icon :icon="['fas', 'chevron-up']" class="text-[24px] text-[#6B6B6B]" />
                                     </button>
-                                    <button  @click="() => setHeightEditor(null)" v-if="paneEditor < 1.5 || EditorHidden == true"  class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
+                                    <button  @click="() => setHeightEditor(null)" v-show="paneEditor < 1.5 || EditorHidden == true"  class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                         <font-awesome-icon :icon="['fas', 'chevron-down']" class="text-[24px] text-[#6B6B6B]" />
                                     </button>
                                 </div>
                             </div>
+
                             <div class="w-full border-b border-[#B0B0B0] h-9 pl-3 pr-1 flex items-center justify-between">
                                 <div class="flex items-center gap-3">
                                     <div class="relative w-fit">
@@ -126,8 +128,8 @@
                                         <font-awesome-icon :icon="['fas', 'rotate-right']" class="text-[20px] text-[#6B6B6B]" />
                                     </button>
                                     <button @click="fullScreen()" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
-                                        <font-awesome-icon :icon="['fas', 'up-right-and-down-left-from-center']" v-if="isFullscreen == false" class="text-[18px] text-[#6B6B6B]" />
-                                        <font-awesome-icon :icon="['fas', 'down-left-and-up-right-to-center']" v-if="isFullscreen == true" class="text-[18px] text-[#6B6B6B]" />
+                                        <font-awesome-icon :icon="['fas', 'up-right-and-down-left-from-center']" v-show="isFullscreen == false" class="text-[18px] text-[#6B6B6B]" />
+                                        <font-awesome-icon :icon="['fas', 'down-left-and-up-right-to-center']" v-show="isFullscreen == true" class="text-[18px] text-[#6B6B6B]" />
                                     </button>
                                 </div>
                             </div>
@@ -141,26 +143,27 @@
 
                     <pane :size="paneTest" class="min-h-[36px]">
                         <div class="w-full h-full bg-[#FFFFFF] border border-[#B0B0B0] rounded-md overflow-hidden drop-shadow-md">
-                            <div class="w-full bg-[#F6F6F6] border-b border-[#B0B0B0] h-9 px-1 flex items-center justify-between">
-                                <div class="flex items-center gap-[2px]">
-                                    <button class="hover:bg-[#e4e4e4] rounded-md h-8 px-2 flex items-center gap-2">
-                                        <font-awesome-icon :icon="['far', 'square-check']" class="text-[20px] text-[#00C7A3]" />
+                            
+                            <div :class="`w-full bg-[#F6F6F6] border-b border-[#B0B0B0] flex ${isVerticalRightMode ? 'h-full justify-between vertical-mode' : 'h-9 items-center justify-between px-1'}`">
+                                <div class="flex h-full items-center gap-[2px]">
+                                    <button :class="`hover:bg-[#e4e4e4] rounded-md flex items-center gap-2 ${isVerticalRightMode ? 'h-fit py-2' : 'h-8 px-2'}`">
+                                        <font-awesome-icon :icon="['far', 'square-check']" :class="`text-[20px] text-[#00C7A3] ${isVerticalRightMode ? 'rotate-90' : null}`" />
                                         <span class="text-[#000000] text-[16px] font-medium">Test Case</span>
                                     </button>
 
                                     <div class="h-5 w-[1px] bg-[#B0B0B0]"></div>
 
-                                    <button class="hover:bg-[#e4e4e4] rounded-md h-8 px-2 flex items-center gap-2">
-                                        <font-awesome-icon :icon="['fas', 'terminal']" class="text-[20px] text-[#7FDBC1]" />
+                                    <button :class="`hover:bg-[#e4e4e4] rounded-md h-8 px-2 flex items-center gap-2 ${isVerticalRightMode ? 'h-fit py-2' : 'h-8 px-2'}`">
+                                        <font-awesome-icon :icon="['fas', 'terminal']" class="`text-[20px] text-[#7FDBC1] ${isVerticalRightMode ? 'rotate-90' : null}`" />
                                         <span class="text-[#6B6B6B] text-[16px] font-light">Test Result</span>
                                     </button>
                                 </div>
 
                                 <div class="flex items-center gap-[4px]">
-                                    <button @click="setHeightTest(0)" v-if="paneTest > 2 && TestHidden == false" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
+                                    <button @click="setHeightTest(0)" v-show="paneTest > 2 && TestHidden == false" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                         <font-awesome-icon :icon="['fas', 'chevron-up']" class="text-[24px] text-[#6B6B6B]" />
                                     </button>
-                                    <button @click="setHeightTest(null)" v-if="paneTest < 2  || TestHidden == true" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
+                                    <button @click="setHeightTest(null)" v-show="paneTest < 2  || TestHidden == true" class="hover:bg-[#e4e4e4] rounded-md h-8 w-8 flex items-center justify-center">
                                         <font-awesome-icon :icon="['fas', 'chevron-down']" class="text-[24px] text-[#6B6B6B]" />
                                     </button>
                                 </div>
@@ -180,6 +183,8 @@ const isLangExpanded = ref(false);
 const isVersionExpanded = ref(false);
 
 const isVerticalLeftMode = ref(false);
+const isVerticalRightMode = ref(false);
+
 
 const paneLeftWidth = ref(50);
 const paneLeftWidthOld = ref(0);
@@ -198,6 +203,7 @@ const TestHidden = ref(false);
 
 
 const resizedHeight = (sizes) => {
+    nextTick(() => {
     //change height of pane
 
     paneEditorOld.value = sizes[0].size;
@@ -218,35 +224,39 @@ const resizedHeight = (sizes) => {
     }else{
         TestHidden.value = false;
     }
+    });
 }
 
 function setHeightEditor(value) {
-    if(value != null && paneEditorOld.value == 0 && paneTestOld.value == 0){
-        paneEditorOld.value = paneEditor.value;
-        paneTestOld.value = paneTest.value;
-    }
-
-    if (value != null) {
-        paneEditor.value = value;
-        paneTest.value = 100 - value;
-    }else{
-
-        if(paneEditorOld.value < 6 || EditorHidden.value == true){
-            paneEditor.value = 60;
-            paneTest.value = 40;
-            EditorHidden.value = false;
-
-            console.log('1');
-        }else if (paneEditorOld.value > 6 ||  paneTestOld.value > 6){
-            paneEditor.value = paneEditorOld.value;
-            paneTest.value = paneTestOld.value;
-
-            console.log('2');
+    nextTick(() => {
+        if(value != null && paneEditorOld.value == 0 && paneTestOld.value == 0){
+            paneEditorOld.value = paneEditor.value;
+            paneTestOld.value = paneTest.value;
         }
-    }
+
+        if (value != null) {
+            paneEditor.value = value;
+            paneTest.value = 100 - value;
+        }else{
+
+            if(paneEditorOld.value < 6 || EditorHidden.value == true){
+                paneEditor.value = 60;
+                paneTest.value = 40;
+                EditorHidden.value = false;
+
+                console.log('1');
+            }else if (paneEditorOld.value > 6 ||  paneTestOld.value > 6){
+                paneEditor.value = paneEditorOld.value;
+                paneTest.value = paneTestOld.value;
+
+                console.log('2');
+            }
+        }
+    });
 }
 
 function setHeightTest(value) {
+    nextTick(() => {
     if(value != null && paneEditorOld.value == 0 && paneTestOld.value == 0){
         paneEditorOld.value = paneEditor.value;
         paneTestOld.value = paneTest.value;
@@ -276,6 +286,7 @@ function setHeightTest(value) {
         }
         
     }
+    });
 }
 
 
@@ -284,6 +295,15 @@ watch(paneLeftWidth, (value) => {
         isVerticalLeftMode.value = true;
     }else {
         isVerticalLeftMode.value = false;
+    }
+
+});
+
+watch(paneRightWidth, (value) => {
+    if (value <= 1.5) {
+        isVerticalRightMode.value = true;
+    }else {
+        isVerticalRightMode.value = false;
     }
 
 });
@@ -299,6 +319,7 @@ const onResizedWidth = (sizes) => {
         paneRightWidth.value = sizes[1].size;
       
         isVerticalLeftMode.value = sizes[0].size <= 1.5;
+        isVerticalRightMode.value = sizes[1].size <= 1.5;
     } else {
       console.log('No sizes data available');
     }
@@ -307,6 +328,7 @@ const onResizedWidth = (sizes) => {
 
 
 function setWidthPaneLeft(value) {
+    nextTick(() => {
     if(value != null && paneLeftWidthOld.value == 0 && paneRightWidthOld.value == 0){
         paneLeftWidthOld.value = paneLeftWidth.value;
         paneRightWidthOld.value = paneRightWidth.value;
@@ -336,17 +358,21 @@ function setWidthPaneLeft(value) {
         }
         
     }
+    });
 }
 
 const isFullscreen = ref(false);
 function fullScreen() {
-  const fullscreen = document.querySelector("#main_screen");
-
-  if (!isFullscreen.value) fullscreen.requestFullscreen();
-  else document.exitFullscreen();
-
-  isFullscreen.value = !isFullscreen.value;
+  nextTick(() => {
+    const fullscreenElement = document.querySelector("#main_screen");
+    if (fullscreenElement) {
+      fullscreenElement.requestFullscreen();
+    } else {
+      console.error("Element not found");
+    }
+  });
 }
+
 
 
 </script>
