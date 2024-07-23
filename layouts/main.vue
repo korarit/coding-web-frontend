8347ef59c16f90d254a86ba4807379b84ef427f5
korarit <krtza1@gmail.com>
2024-07-23 22:45:31 +0700

    add modal login

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,12 +1,31 @@
 <template>
-    <div class="h-fit flex flex-col">
-    <Navbar />
+    <div class="h-[100dvh] flex flex-col max-w-full" :class="show_login_modal ? 'overflow-hidden' : ''">
+        <Navbar @open-login="() => {show_login_modal = true; showModal}" />
 
-        <div class="bg-[#FBFBFB]">
-            <slot>
-            </slot>
+            <div class="bg-[#FBFBFB]">
+                <slot>
+                </slot>
+            </div>
+                
+        <Footer />
+        <div v-show="show_login_modal" class="absolute min-w-full h-[100dvh] top-0 left-0">
+            <ModalLogin :show="show_login_modal" @close-modal="show_login_modal = false" />
         </div>
-            
-    <Footer />
     </div>
 </template>
+
+
+<style scoped>
+body {
+    overflow-y: hidden;
+}
+</style>
+<script setup>
+
+///////////////////////// modal control /////////////////////////
+const show_login_modal = ref(false)
+
+const test = () => {
+    console.log('test')
+}
+</script>
