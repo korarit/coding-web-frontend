fe51a9887f1f44afd7636807d144a15512d62319
korarit <krtza1@gmail.com>
2024-07-24 23:49:08 +0700

    add function for test concept coding for prepare make login system

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="h-[100dvh] flex flex-col max-w-full" :class="show_login_modal ? 'overflow-hidden' : ''">
-        <Navbar @open-login="() => {show_login_modal = true; showModal}" />
+        <Navbar 
+            :login_status="status_login"
+            @open-login="() => {show_login_modal = true;}"
+            @open-user="() => {show_user_modal = !show_user_modal;}"
+        />
 
             <div class="bg-[#FBFBFB]">
                 <slot>
@@ -9,7 +13,15 @@
                 
         <Footer />
         <div v-show="show_login_modal" class="absolute min-w-full h-[100dvh] top-0 left-0">
-            <ModalLogin :show="show_login_modal" @close-modal="show_login_modal = false" />
+            <ModalLogin 
+                :show="show_login_modal" 
+                @close-modal="show_login_modal = false" 
+                @login="login"
+            />
+        </div>
+
+        <div class="absolute w-fit h-fit top-20 xl:right-16  2xl:right-[96px] hidden xl:block">
+            <ModalUserData :show="show_user_modal" />
         </div>
     </div>
 </template>
@@ -24,8 +36,12 @@ body {
 
 ///////////////////////// modal control /////////////////////////
 const show_login_modal = ref(false)
+const show_user_modal = ref(false)
 
-const test = () => {
-    console.log('test')
+const status_login = ref(false)
+
+const login = (username, password) => {
+    console.log(username, password)
+    status_login.value = true
 }
 </script>
