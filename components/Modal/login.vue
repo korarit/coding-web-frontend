a652848a3c0ccb8aa7083ed60debcd472aaffb38
korarit <krtza1@gmail.com>
2024-07-25 22:55:53 +0700

    update add login by user/email and add dark mode

--- components/Modal/login.vue
+++ components/Modal/login.vue
@@ -9,17 +9,17 @@
     leave-from-class="opacity-100 translate-y-0"
     leave-to-class="opacity-0 translate-y-full"
   >
-    <div v-show="show_modal" class="bg-white p-6 rounded-lg shadow-lg w-[90dvw] sm:w-96 2xl:w-[24dvw]">
+    <div v-show="show_modal" class="bg-white dark:bg-[#3D3D3D] dark:border-[#676767] p-6 rounded-lg shadow-lg w-[90dvw] sm:w-96 2xl:w-[24dvw]">
       <div class="flex justify-between">
         <div>
           <h2 class="text-[26px] lg:text-[34px] xl:text-[32px] 2xl:text-[36px] text-[#00c7a3]">
             ยินดีต้อนรับกลับมา!
           </h2>
-          <p class="text-[22px] 2xl:text-[28px] text-[#606060] mb-4">เข้าสู่ระบบบัญชีของคุณ</p>
+          <p class="text-[22px] 2xl:text-[28px] text-[#606060] dark:text-[#FEFEFE] mb-4">เข้าสู่ระบบบัญชีของคุณ</p>
         </div>
         <button class="flex items-center justify-center w-[15%]" @click="closeModal">
           <font-awesome-icon :icon="['fas', 'xmark']"
-            class="text-[34px] lg:text-[42px] xl:text-[40px] 2xl:text-[56px] text-[#606060] hover:text-[#000000] active:text-black" />
+            class="text-[34px] lg:text-[42px] xl:text-[40px] 2xl:text-[56px] text-[#606060] hover:text-[#000000] active:text-black dark:text-[#FEFEFE] dark:hover:text-[#b0b0b0] dark:active:text-black" />
         </button>
       </div>
 
@@ -27,7 +27,7 @@
           <label for="username" class="sr-only">Username / Email</label>
           <input v-model="username"
             type="text" id="username" placeholder="Username / Email"
-            class="w-full p-2 border rounded hover:border-gray-500 2xl:text-[20px] placeholder:font-light" 
+            class="w-full p-2 border rounded hover:border-gray-500 2xl:text-[20px] placeholder:font-light dark:bg-[#262626] dark:border-[#626262] dark:text-white" 
             :class="`${username_error ? 'border-red-500' : 'border-gray-300'}`"
           />
           <p v-show="username_error" class="text-red-500 text-[12px] 2xl:text-[16px]">กรุณากรอกชื่อผู้ใช้</p>
@@ -37,13 +37,13 @@
           <input
             v-model="password"
             type="password" id="password" placeholder="Password"
-            class="w-full p-2 border border-gray-300 rounded hover:border-gray-500 2xl:text-[20px] placeholder:font-light"
+            class="w-full p-2 border border-gray-300 rounded hover:border-gray-500 2xl:text-[20px] placeholder:font-light dark:bg-[#262626] dark:border-[#626262] dark:text-white"
             :class="`${password_error ? 'border-red-500' : 'border-gray-300'}`"
           />
           <p v-show="password_error" class="text-red-500 text-[12px] 2xl:text-[16px]">กรุณากรอกรหัสผ่าน</p>
         </div>
         <div class="flex items-center justify-between mb-4">
-          <label class="flex items-center text-sm 2xl:text-[20px] text-gray-600">
+          <label class="flex items-center text-sm 2xl:text-[20px] text-gray-600 dark:text-[#FEFEFE]">
             <input type="checkbox" class="mr-2 h-4 w-4" />
             จดจำฉัน
           </label>
@@ -52,25 +52,25 @@
           </p>
         </div>
 
-        <button @click="checkInput" class="w-full bg-[#00C7A3] hover:bg-[#199c80] active:bg-[#199c80] 2xl:text-[24px] text-white p-2 rounded">
+        <button @click="checkInput" class="w-full bg-[#00C7A3] hover:bg-[#199c80] active:bg-[#199c80] dark:bg-[#3DD6BA] dark:hover:bg-[#00C7A3] dark:active:bg-[#00C7A3] 2xl:text-[24px] text-white p-2 rounded">
           เข้าสู่ระบบ
         </button>
         <p class="text-center text-sm 2xl:text-[20px] my-4 text-[#606060]">หรือเข้าสู่ระบบด้วย</p>
         <div class="flex justify-center space-x-4 mb-4">
-          <a href="#" class="text-gray-600 hover:text-black">
+          <a href="#" class="text-gray-600 hover:text-black dark:text-[#FEFEFE] dark:hover:text-gray-400 dark:active:text-gray-400">
             <font-awesome-icon :icon="['fab', 'github']" class="text-[36px]" />
           </a>
-          <a href="#" class="text-gray-600 hover:text-black">
+          <a href="#" class="text-gray-600 hover:text-black dark:text-[#FEFEFE] dark:hover:text-gray-400 dark:active:text-gray-400">
             <font-awesome-icon :icon="['fab', 'facebook']" class="text-[36px]" />
           </a>
-          <a href="#" class="text-gray-600 hover:text-black">
+          <a href="#" class="text-gray-600 hover:text-black dark:text-[#FEFEFE] dark:hover:text-gray-400 dark:active:text-gray-400">
             <font-awesome-icon :icon="['fab', 'google']" class="text-[36px]" />
           </a>
-          <a href="#" class="text-gray-600 hover:text-black">
+          <a href="#" class="text-gray-600 hover:text-black dark:text-[#FEFEFE] dark:hover:text-gray-400 dark:active:text-gray-400">
             <font-awesome-icon :icon="['fab', 'windows']" class="text-[36px]" />
           </a>
         </div>
-        <p class="text-center text-sm 2xl:text-[20px] text-[#606060]">
+        <p class="text-center text-sm 2xl:text-[20px] text-[#606060] dark:text-[#FEFEFE]">
           ยังไม่มีบัญชีหรือ
           <a href="#" class="text-blue-500 hover:text-blue-700 underline underline-offset-2">สมัคร</a>
           เลย
@@ -86,7 +86,7 @@
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 }
 </style>
-<script setup>
+<script setup lang="ts">
 
 const props = defineProps({
   show: Boolean
@@ -124,7 +124,8 @@ watch(password, (val) => {
   }
 })
 
-const checkInput = () => {
+const { signIn } = useAuth()
+const checkInput = async () => {
   if (username.value === '' || password.value === '') {
     if (username.value === '') {
       username_error.value = true
@@ -137,6 +138,20 @@ const checkInput = () => {
     password_error.value = false
     emit('login', username.value, password.value)
 
+    let type = 'email'
+    if (username.value.includes('@')) {
+      type = 'email';
+    } else {
+      type = 'username';
+    }
+
+    const login_data = {
+      type: type,
+      username: username.value,
+      password: password.value
+    }
+
+    await signIn('credentials',login_data)
     closeModal()
   }
 }
