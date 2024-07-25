2637739933dec1e01971cfea7785266c1c1e377b
korarit <krtza1@gmail.com>
2024-07-25 22:53:23 +0700

    config nuxt auth sidebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,37 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/golbal.css','@fortawesome/fontawesome-svg-core/styles.css'],
+  css: ['~/assets/css/golbal.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
 
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  runtimeConfig:{
+    apiBase: process.env.API_BASE_URL || 'http://localhost:3000',
+    oauth:{
+      google:{
+        clientId: process.env.GOOGLE_CLIENT_ID || 'GOOGLE_ID',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'GOOGLE_SECRET'
+      },
+      facebook:{
+        clientId: process.env.FACEBOOK_CLIENT_ID || 'FACEBOOK_ID',
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET || 'FACEBOOK_SECRET'
+      },
+      azureAD:{
+        clientId: process.env.AZURE_AD_CLIENT_ID || 'AZURE_AD_ID',
+        clientSecret: process.env.AZURE_AD_CLIENT_SECRET || 'AZURE_AD_SECRET',
+        tenantId: process.env.AZURE_AD_TENANT_ID || 'AZURE_AD_TENANT_ID'
+      },
+      github:{
+        clientId: process.env.GITHUB_CLIENT_ID || 'GITHUB_ID',
+        clientSecret: process.env.GITHUB_CLIENT_SECRET || 'GITHUB_SECRET'
+      }
+    }
   },
   
   modules: [
@@ -24,7 +48,11 @@ export default defineNuxtConfig({
   },
 
   auth: {
-    isEnabled: false,
+    provider: {
+      type: 'authjs',
+      trustHost: false,
+      defaultProvider: 'credentials',
+      addDefaultCallbackUrl: true
+    }
   }
-
 })
