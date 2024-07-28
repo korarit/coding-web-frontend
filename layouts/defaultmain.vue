58ff0696b050be52685d8effbfc857da5890444f
korarit <krtza1@gmail.com>
2024-07-28 23:05:45 +0700

    make modal login to connect useLoginModalControl

---
+++ layouts/defaultmain.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="h-[100dvh] flex flex-col max-w-full" :class="show_login_modal ? 'overflow-hidden' : ''">
+        <Navbar 
+            :login_status="status"
+            @open-login="open_modal_login"
+            @open-user="() => {show_user_modal = !show_user_modal;}"
+
+            :profile="data?.profile_img"
+        />
+            <div class="bg-[#FBFBFB]">
+                <slot>
+                </slot>
+            </div>
+                
+        <Footer />
+        <div v-show="show_login_modal" class="absolute min-w-full h-[100dvh] top-0 left-0">
+            <ModalLogin 
+                :show="show_login_modal" 
+                @close-modal="close_modal_login" 
+                @login="login"
+            />
+        </div>
+
+        <div class="absolute w-fit h-fit top-20 xl:right-16  2xl:right-[96px] hidden xl:block">
+            <ModalUserData :show="show_user_modal" :data="data" />
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+body {
+    overflow-y: hidden;
+}
+</style>
+<script setup>
+
+///////////////////////// modal control /////////////////////////
+const show_user_modal = ref(false)
+
+const status_login = ref(false)
+
+
+
+
+
+const {run, open_modal_login, close_modal_login} = useLoginModalControl()
+const show_login_modal = run()
+
+///////////////////////// login /////////////////////////
+
+const login = (username, password) => {
+    console.log(username, password)
+    status_login.value = true
+}
+
+///////////////////////// login status /////////////////////////
+
+const { status , data } = useAuth()
+</script>
