448109b47f17ac0708818eb5bbee30ebcd5d9762
korarit <krtza1@gmail.com>
2024-07-29 00:39:51 +0700

    make dark mode

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-w-full bg-[#FEFEFE] border-b border-[#cecece] drop-shadow-md py-3 flex justify-between px-4 xl:px-16 2xl:px-[96px]">
+    <div class="min-w-full bg-[#FEFEFE] dark:bg-[#3D3D3D] border-b border-[#cecece] dark:border-[#3A3A3A] drop-shadow-md py-3 flex justify-between px-4 xl:px-16 2xl:px-[96px]">
 
         <!-- logo -->
         <div>
@@ -8,22 +8,22 @@
 
         <!-- menu -->
         <div class="items-center w-fit gap-x-5 hidden xl:flex">
-            <p class="text-[20px] text-[#00C7A3] text-center"> 
+            <p class="text-[20px] text-[#00C7A3] dark:text-[#3DD6BA] text-center"> 
                 หน้าหลัก
             </p>
-            <p class="text-[20px] text-[#606060] font-normal text-center"> 
+            <p class="text-[20px] text-[#606060] dark:text-[#FEFEFE] font-normal text-center"> 
                 โจทย์
             </p>
-            <p class="text-[20px] text-[#606060] font-normal text-center"> 
+            <p class="text-[20px] text-[#606060] dark:text-[#FEFEFE] font-normal text-center"> 
                 การแข่ง
             </p>
-            <p class="text-[20px] text-[#606060] font-normal text-center"> 
+            <p class="text-[20px] text-[#606060] dark:text-[#FEFEFE] font-normal text-center"> 
                 ชั้นเรียน
             </p>
-            <button v-if="props.login_status != 'authenticated'" class="rounded-md drop-shadow-md bg-[#606060] hover:bg-[#303030] active:bg-[#303030] text-[20px] text-white font-medium px-3 py-[4px]">
+            <button v-if="props.login_status != 'authenticated'" class="rounded-md drop-shadow-md bg-[#606060] dark:bg-[#FEFEFE] hover:bg-[#303030] dark:hover:bg-[#a0a0a0] active:bg-[#303030] dark:active:bg-[#a0a0a0] text-[20px] text-white dark:text-[#0F0F0F] font-medium px-3 py-[4px]">
                 Sing Up
             </button>
-            <button v-if="props.login_status  != 'authenticated'" @click="$emit('openLogin')" class="rounded-md drop-shadow-md bg-[#00C7A3] hover:bg-[#199c80] active:bg-[#199c80] text-[20px] text-white font-medium px-3 py-[4px]">
+            <button v-if="props.login_status  != 'authenticated'" @click="$emit('openLogin')" class="rounded-md drop-shadow-md bg-[#00C7A3] dark:bg-[#3DD6BA] hover:bg-[#199c80] dark:hover:bg-[#00C7A3] active:bg-[#199c80] dark:active:bg-[#00C7A3] text-[20px] text-white dark:text-[#0F0F0F] font-medium px-3 py-[4px]">
                 Log In
             </button>
             <button v-if="props.login_status  == 'authenticated'" @click="$emit('openUser')" class="flex items-center">
@@ -34,7 +34,7 @@
         </div>
         <!-- mobile menu -->
         <div class="flex xl:hidden">
-            <button class="text-[42px] text-[#00C7A3] hover:text-[#199c80] active:text-[#199c80] font-medium px-3 flex items-center">
+            <button class="text-[42px] text-[#00C7A3] dark:text-[#3DD6BA] hover:text-[#199c80] dark:hover:text-[#00C7A3] active:text-[#199c80] dark:active:text-[#00C7A3] font-medium px-3 flex items-center">
                 <font-awesome-icon :icon="['fas', 'bars']" />
             </button>
         </div>
--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="min-w-full py-4 md:py-12 px-4 xl:px-16 2xl:px-[96px] bg-[#FFFFFF] shadow-[inset_0_3px_10px_-7px_rgba(0,0,0,0.5)] border-t">
+    <div class="min-w-full py-2 md:py-8 px-4 xl:px-16 2xl:px-[96px] bg-[#FFFFFF] dark:bg-[#3D3D3D] shadow-[inset_0_3px_10px_-7px_rgba(0,0,0,0.5)] border-t border-[#0000000f]">
         <div class="min-w-full flex flex-col md:flex-row md:items-center  md:justify-between">
             <div>
-                <p class="text-[5vw] md:text-[24px] xl:text-[24px] 2xl:text-[28px]">
+                <p class="dark:text-[#FEFEFE] text-[5vw] md:text-[24px] xl:text-[24px] 2xl:text-[28px]">
                     © 2024 - 2025 รวมพลคนนอนเยอะ team
                 </p>
-                <p class="text-[4vw] md:text-[16px] xl:text-[20px] 2xl:text-[24px]">
+                <p class="dark:text-[#FEFEFE] text-[4vw] md:text-[16px] xl:text-[20px] 2xl:text-[24px]">
                     Project is in the subject of Software engineer major University of Phayao
                 </p>  
             </div>
