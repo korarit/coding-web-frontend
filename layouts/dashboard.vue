9ff64fb2c40ff0951140dd0243764c6b88c6a936
korarit <krtza1@gmail.com>
2024-09-08 02:38:30 +0700

    add template layout of dashboard for admin

---
+++ layouts/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex h-dvh max-w-full bg-[#FBFBFB] dark:bg-[#0F0F0F]">
+        <div class="w-[72px]">
+            <Sidebar />
+        </div>
+        <div class="flex-auto max-h-full px-16 pt-12" :class="show_modal ? 'overflow-hidden' : 'overflow-y-auto'">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const {statusModal} = useModalControl()
+const show_modal = statusModal()
+</script>
