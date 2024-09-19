b4c2a8d8c4381454fbd943f9099dbfd12fc9fbcc
66022578 <150765865+66022578@users.noreply.github.com>
2024-09-19 22:40:02 +0700

    New notification_page

---
+++ pages/notification.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="h-screen flex bg-[#FBFBFB] dark:bg-[#0F0F0F]">
+    <Navleft />
+    <div
+      class="w-[80%] 2xl:w-[20%] xl:w-[25%] lg:w-[30%] md:w-[35%] sm:w-[40%] h-fit m-5 border-2 rounded-xl drop-shadow-lg bg-[#00C7A3] dark:bg-[#3DD6BA] border-[#000000] dark:border-[#545454]"
+    >
+      <h1
+        class="flex justify-between p-3 dark:text-[#FEFEFE] 2xl:text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] text-[14px]"
+      >
+        Nottification
+        <button><font-awesome-icon :icon="['fas', 'xmark']" /></button>
+      </h1>
+      <hr class="border-2 border-[#000000] dark:border-[#545454]" />
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] p-1"></div>
+      <div
+        class="flex bg-[#FFFFFF] dark:bg-[#2E2E2E] hover:bg-[#9ba7a4] active:bg-[#b2b7b7] dark:hover:bg-[#4d5553] dark:active:bg-[#7e8583]"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'bullhorn']"
+          class="dark:text-[#F30000] my-auto px-2 2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]"
+        />
+        <button
+          class="pb-2 pt-2.5 2xl:text-[22px] xl:text-[20px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px] dark:text-[#FEFEFE]"
+        >
+          Welcome new arrival 🙂
+          <hr class="pb-2 border-1 border-[#000000]" />
+          <hr class="border-1 border-[#000000]" />
+        </button>
+      </div>
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] p-1"></div>
+      <div
+        class="flex bg-[#FFFFFF] dark:bg-[#2E2E2E] hover:bg-[#9ba7a4] active:bg-[#b2b7b7] dark:hover:bg-[#4d5553] dark:active:bg-[#7e8583]"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'bullhorn']"
+          class="dark:text-[#F30000] my-auto px-2 2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]"
+        />
+        <button
+          class="pb-2 pt-2.5 dark:text-[#FEFEFE] 2xl:text-[22px] xl:text-[20px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]"
+        >
+          New Event For You <span class="text-[#FF0000]">!!</span>
+          <hr class="pb-2 border-1 border-[#000000]" />
+          <hr class="border-1 border-[#000000]" />
+        </button>
+      </div>
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] p-1"></div>
+      <div
+        class="flex bg-[#FFFFFF] dark:bg-[#2E2E2E] hover:bg-[#9ba7a4] active:bg-[#b2b7b7] dark:hover:bg-[#4d5553] dark:active:bg-[#7e8583]"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'bullhorn']"
+          class="dark:text-[#F30000] my-auto px-2 2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]"
+        />
+        <button
+          class="pb-2 pt-2.5 dark:text-[#FEFEFE] 2xl:text-[22px] xl:text-[20px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]"
+        >
+          New Event For You <span class="text-[#FF0000]">!!</span>
+          <hr class="pb-2 border-1 border-[#000000]" />
+          <hr class="border-1 border-[#000000]" />
+        </button>
+      </div>
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] p-1"></div>
+      <div
+        class="flex bg-[#FFFFFF] dark:bg-[#2E2E2E] hover:bg-[#9ba7a4] active:bg-[#b2b7b7] dark:hover:bg-[#4d5553] dark:active:bg-[#7e8583]"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'bullhorn']"
+          class="dark:text-[#F30000] my-auto px-2 2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]"
+        />
+        <button
+          class="pb-2 pt-2.5 dark:text-[#FEFEFE] 2xl:text-[22px] xl:text-[20px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]"
+        >
+          New Event For You <span class="text-[#FF0000]">!!</span>
+          <hr class="pb-2 border-1 border-[#000000]" />
+          <hr class="border-1 border-[#000000]" />
+        </button>
+      </div>
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] p-1"></div>
+      <div
+        class="flex bg-[#FFFFFF] dark:bg-[#2E2E2E] hover:bg-[#9ba7a4] active:bg-[#b2b7b7] dark:hover:bg-[#4d5553] dark:active:bg-[#7e8583]"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'bullhorn']"
+          class="dark:text-[#F30000] my-auto px-2 2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]"
+        />
+        <button
+          class="pb-2 pt-2.5 dark:text-[#FEFEFE] 2xl:text-[22px] xl:text-[20px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]"
+        >
+          New Event For You <span class="text-[#FF0000]">!!</span>
+          <hr class="pb-2 border-1 border-[#000000]" />
+          <hr class="border-1 border-[#000000]" />
+        </button>
+      </div>
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] p-1"></div>
+      <div
+        class="flex bg-[#FFFFFF] dark:bg-[#2E2E2E] hover:bg-[#9ba7a4] active:bg-[#b2b7b7] dark:hover:bg-[#4d5553] dark:active:bg-[#7e8583]"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'bullhorn']"
+          class="dark:text-[#F30000] my-auto px-2 2xl:text-[20px] xl:text-[18px] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]"
+        />
+        <button
+          class="pb-2 pt-2.5 dark:text-[#FEFEFE] 2xl:text-[22px] xl:text-[20px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]"
+        >
+          New Event For You <span class="text-[#FF0000]">!!</span>
+          <hr class="pb-2 border-1 border-[#000000]" />
+          <hr class="border-1 border-[#000000]" />
+        </button>
+      </div>
+
+      <div class="bg-[#D9D9D9] dark:bg-[#262626] rounded-b-xl p-2"></div>
+    </div>
+  </div>
+</template>
